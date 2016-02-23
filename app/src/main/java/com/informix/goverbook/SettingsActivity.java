ba051f9caaab01141677acf5161cd19ec0a7471f
John Fog <jdevil@bk.ru>
2016-02-23 20:40:45 +0900

    Добавлена выборка по улусам

--- app/src/main/java/com/informix/goverbook/SettingsActivity.java
+++ app/src/main/java/com/informix/goverbook/SettingsActivity.java
@@ -14,20 +14,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.informix.goverbook.DBHelper;import com.informix.goverbook.Decompress;import com.informix.goverbook.DownloadTask;import com.informix.goverbook.R;import org.json.JSONArray;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;import java.lang.Character;import java.lang.Exception;import java.lang.Override;import java.lang.String;import java.lang.StringBuilder;import java.lang.Void;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.lang.Character;import java.lang.Exception;import java.lang.Override;import java.lang.String;import java.lang.StringBuilder;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
     DBHelper dbHelper;
     Button btnUpdatedb;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,8 +90,8 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
 
 
                 //new Decompress().unzip(getCacheDir() + "/base.zip", getCacheDir() + "");
-
-                new ParseTask(SettingsActivity.this, "Пересоздаю базу").execute();
+                File mTargetFile = new File(getCacheDir() + "/base.zip");
+                new ParseTask(SettingsActivity.this, mTargetFile, "Качаю").execute("http://www.rcitsakha.ru/rcit/zz/base.zip");
 
 
         }
@@ -91,21 +99,26 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
     }
 
 
-    private class ParseTask extends AsyncTask<Void, Void, String> {
+    private class ParseTask extends AsyncTask<String, Integer, String> {
 
         String resultJson;
         private ProgressDialog mPDialog;
         private Context mContext;
+        private File mTargetFile;
+        private boolean isDownloaded = false;
 
-        public ParseTask(Context context, String dialogMessage) {
+        public ParseTask(Context context, File targetFile, String dialogMessage) {
             this.mContext = context;
+            this.mTargetFile = targetFile;
 
             mPDialog = new ProgressDialog(context);
 
             mPDialog.setMessage(dialogMessage);
             mPDialog.setIndeterminate(true);
-            mPDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mPDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
             mPDialog.setCancelable(true);
+            mPDialog.setMax(100);
+
 
             //final ParseTask me = this;
 //            mPDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
@@ -121,18 +134,69 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         //Создаем запрос и получаем JSON ответ для нашиих баз
 
         protected void onPreExecute() {
-            File destinationFile = new File(getCacheDir() + "/base.zip");
-            new DownloadTask(SettingsActivity.this, destinationFile, "Качаю", getCacheDir().toString()).execute("http://www.rcitsakha.ru/rcit/zz/base.zip");
-            //mPDialog.show();
+            mPDialog.show();
         }
 
-        protected String doInBackground(Void... params) {
+        protected String doInBackground(String... sUrl) {
+            InputStream input = null;
+            OutputStream output = null;
+            HttpURLConnection connection = null;
+            try {
+                URL url = new URL(sUrl[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                // expect HTTP 200 OK, so we don't mistakenly save error report
+                // instead of the file
+                if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    return "Server returned HTTP " + connection.getResponseCode()
+                            + " " + connection.getResponseMessage();
+                }
+                Log.i("DownloadTask","Response " + connection.getResponseCode());
+
+                // this will be useful to display download percentage
+                // might be -1: server did not report the length
+                int fileLength = connection.getContentLength();
+
+                // download the file
+                input = connection.getInputStream();
+                output = new FileOutputStream(mTargetFile,false);
+
+                byte data[] = new byte[4096];
+                long total = 0;
+                int count;
+                while ((count = input.read(data)) != -1) {
+                    // allow canceling with back button
+                    if (isCancelled()) {
+                        Log.i("DownloadTask","Cancelled");
+                        input.close();
+                        return null;
+                    }
+                    total += count;
+                    // publishing the progress....
+                    if (fileLength > 0) // only if total length is known
+                        publishProgress((int) (total * 100 / fileLength));
+                    output.write(data, 0, count);
+                }
+            } catch (Exception e) {
+                return e.toString();
+            } finally {
+                try {
+                    if (output != null)
+                        output.close();
+                    if (input != null)
+                        input.close();
+                } catch (IOException ignored) {
+                }
+
+                if (connection != null)
+                    connection.disconnect();
+            }
+
             new Decompress().unzip(getCacheDir() + "/base.zip", getCacheDir() + "");
             resultJson = getJSON(getCacheDir() + "/base.json");
-            return resultJson;
-        }
 
-        protected void onPostExecute(String strJson) {
+            //Вносим изменения в базу
             JSONObject dataJsonObj = null;
             try {
                 dataJsonObj = new JSONObject(resultJson);
@@ -142,7 +206,9 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
             SQLiteDatabase database = dbHelper.getWritableDatabase();
             ContentValues contentValues = new ContentValues();
             ContentValues contentValuesForOnMain = new ContentValues();
+            isDownloaded = true;
 
+            publishProgress((int) (0));
             database.beginTransaction();
             try {
                 dataJsonObj = new JSONObject(resultJson);
@@ -173,8 +239,9 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                 database.endTransaction();
             }
 
-                contentValues.clear();
+            contentValues.clear();
 
+            publishProgress((int) (20));
             database.beginTransaction();
             try {
                 //Заполняем базу Пользователей s_org
@@ -195,7 +262,7 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
 
 
                     if (((jsonorgs.getString("TYPEID")) == "null") || (jsonorgs.getString("TYPEID").equals("0"))) {
-                                contentValues.put(DBHelper.KEY_TYPEID, "6");
+                        contentValues.put(DBHelper.KEY_TYPEID, "6");
                     }
                     else
                     if (Integer.parseInt(jsonorgs.getString("TYPEID")) > 100) {
@@ -205,9 +272,9 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                         database.insert(DBHelper.TABLE_ONMAIN, null, contentValuesForOnMain);
                     }
                     else
-                        {
-                            contentValues.put(DBHelper.KEY_TYPEID, jsonorgs.getInt("TYPEID"));
-                        }
+                    {
+                        contentValues.put(DBHelper.KEY_TYPEID, jsonorgs.getInt("TYPEID"));
+                    }
 
                     database.insert(DBHelper.TABLE_ORG, null, contentValues);
                 }
@@ -220,8 +287,9 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                 database.endTransaction();
             }
 
-                contentValues.clear();
+            contentValues.clear();
 
+            publishProgress((int) (40));
             database.beginTransaction();
             try {
                 //Заполняем базу Пользователей s_areas
@@ -235,9 +303,9 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                     if (jsonorgs.getString(DBHelper.KEY_SORTING) != "null") {
                         contentValues.put(DBHelper.KEY_SORTING, jsonorgs.getInt(DBHelper.KEY_SORTING));
                     } else
-                    contentValues.put(DBHelper.KEY_SORTING, "");
+                        contentValues.put(DBHelper.KEY_SORTING, "");
 
-                database.insert(DBHelper.TABLE_AREAS, null, contentValues);
+                    database.insert(DBHelper.TABLE_AREAS, null, contentValues);
 
                 }
                 database.setTransactionSuccessful();
@@ -249,8 +317,9 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
             }
 
 
-                contentValues.clear();
+            contentValues.clear();
 
+            publishProgress((int) (60));
             database.beginTransaction();
             try {
                 //Заполняем базу Пользователей s_depart
@@ -264,12 +333,12 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                     if ((jsonorgs.getString(DBHelper.KEY_ORGID) != "null")) {
                         contentValues.put(DBHelper.KEY_ORGID, jsonorgs.getInt(DBHelper.KEY_ORGID));
                     } else
-                    contentValues.put(DBHelper.KEY_ORGID, "");
+                        contentValues.put(DBHelper.KEY_ORGID, "");
 
                     if ((jsonorgs.getString(DBHelper.KEY_SORTING) != "null")) {
                         contentValues.put(DBHelper.KEY_SORTING, jsonorgs.getInt(DBHelper.KEY_SORTING));
                     } else
-                    contentValues.put(DBHelper.KEY_SORTING, "");
+                        contentValues.put(DBHelper.KEY_SORTING, "");
 
                     database.insert(DBHelper.TABLE_DEPART, null, contentValues);
 
@@ -282,7 +351,9 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                 database.endTransaction();
             }
 
-                contentValues.clear();
+            contentValues.clear();
+
+            publishProgress((int) (80));
             database.beginTransaction();
             try {
                 //Заполняем базу Пользователей s_otype
@@ -304,14 +375,28 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
 
             contentValues.clear();
             database.close();
+            return String.valueOf(RESULT_OK);
+        }
 
+        protected void onPostExecute(String strJson) {
             Toast toast = Toast.makeText(SettingsActivity.this, "База успешно обновлена",Toast.LENGTH_LONG);
             toast.show();
-            //mPDialog.dismiss();
+            mPDialog.dismiss();
             Intent output = new Intent();
             output.putExtra("isDbUpdated", "1");
             setResult(RESULT_OK, output);
             finish();
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            super.onProgressUpdate(progress);
+            // if we get here, length is known, now set indeterminate to false
+            mPDialog.setIndeterminate(false);
+            if (isDownloaded) {mPDialog.setMessage("Вношу изменения");}
+            mPDialog.setProgress(progress[0]);
+
 
         }
 
@@ -328,8 +413,6 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                 while ((line = r.readLine()) != null) {
                     total.append(line);
                 }
-
-
                 resultJson = total.toString();
 
             } catch (Exception e) {
@@ -358,8 +441,8 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                 }
             }
 
-        return String.copyValueOf(a);
-    }
+            return String.copyValueOf(a);
+        }
 
         public String modOrgString(String org){
             org=org.replaceAll("&quot;","'");
@@ -368,7 +451,7 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         }
 
 
-  }
+    }
 
 
 }
